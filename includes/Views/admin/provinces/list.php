cb3bc815a35a28002bbccc7b1dc3b15700956c30
4ndrisw <4ndr1sw@gmail.com>
2024-11-07 20:14:51 +0700

    merubah yampilan kiri menjadi panel

--- includes/Views/admin/provinces/list.php
+++ includes/Views/admin/provinces/list.php
@@ -2,14 +2,43 @@
 if (!defined('ABSPATH')) exit;
 ?>
 <div class="wrap ir-provinces">
-    <h1>Daftar Provinsi</h1>
-    
-    <div class="ir-toolbar">
-        <button type="button" class="button button-primary" id="btnAddProvince">
-            Tambah Provinsi
-        </button>
+    <!-- Convert to panel structure -->
+    <div class="ir-panel">
+        <div class="ir-panel-header">
+            <h2>Daftar Provinsi</h2>
+            <div class="ir-panel-actions">
+                <button type="button" class="button button-primary" id="btnAddProvince">
+                    Tambah Provinsi
+                </button>
+            </div>
+        </div>
+        <div class="ir-panel-content">
+            <div class="ir-table-container">
+                <table id="provincesTable" class="display" style="width:100%">
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Nama Provinsi</th>
+                            <th>Jumlah Kab/Kota</th>
+                            <th>Tanggal Dibuat</th>
+                            <th>Aksi</th>
+                        </tr>
+                    </thead>
+                    <tfoot>
+                        <tr>
+                            <th>ID</th>
+                            <th>Nama Provinsi</th>
+                            <th>Jumlah Kab/Kota</th>
+                            <th>Tanggal Dibuat</th>
+                            <th>Aksi</th>
+                        </tr>
+                    </tfoot>
+                </table>
+            </div>
+        </div>
     </div>
-    
+
+    <!-- Modal tetap di luar panel -->
     <div id="provinceModal" class="ir-modal">
         <div class="ir-modal-content">
             <div class="ir-modal-header">
@@ -39,28 +68,5 @@ if (!defined('ABSPATH')) exit;
                 <button type="submit" class="button button-primary" id="btnSaveProvince">Simpan</button>
             </div>
         </div>
-    </div>
-
-    <div class="ir-table-container">
-        <table id="provincesTable" class="display" style="width:100%">
-            <thead>
-                <tr>
-                    <th>ID</th>
-                    <th>Nama Provinsi</th>
-                    <th>Jumlah Kab/Kota</th>
-                    <th>Tanggal Dibuat</th>
-                    <th>Aksi</th>
-                </tr>
-            </thead>
-            <tfoot>
-                <tr>
-                    <th>ID</th>
-                    <th>Nama Provinsi</th>
-                    <th>Jumlah Kab/Kota</th>
-                    <th>Tanggal Dibuat</th>
-                    <th>Aksi</th>
-                </tr>
-            </tfoot>
-        </table>
     </div>
 </div>
