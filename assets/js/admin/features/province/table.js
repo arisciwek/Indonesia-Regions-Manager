bff8a6a4d3e14572588d7ff5c10f68b49d4e4ada
4ndrisw <4ndr1sw@gmail.com>
2024-11-21 14:25:14 +0700

    sebelum penyesuaian detail-panel.js

--- assets/js/admin/features/province/table.js
+++ assets/js/admin/features/province/table.js
@@ -1,7 +1,7 @@
 /**
  * File: assets/js/admin/features/province/table.js
  * Version: 1.1.2
- * Terakhir Diperbarui: 2024-11-20 02:00:00
+ * Terakhir Diperbarui: 2024-11-20 02:23:11
  * Deskripsi: Pengelola tabel provinsi dengan integrasi DataTable dan pengelolaan state
  * 
  * Changelog:
